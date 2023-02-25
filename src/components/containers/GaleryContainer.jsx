07661deafa56c44e620da92fc183594854870b9b
MatiasR <noemail@noemail.com>
2023-02-25 11:00:01 -0300

    Los filtros ahora funcionan, pero con loop infinito entre el useEffect y useState. Branch aun en progreso

--- src/components/containers/GaleryContainer.jsx
+++ src/components/containers/GaleryContainer.jsx
@@ -7,8 +7,80 @@ import GaleryMapper from "../mappers/GaleryMapper";
 import FiltersContainer from "./FiltersContainer";
 // Context
 import { QyParamsCtxtProvider } from "../../context/QyParamsCtxt.jsx";
+//Hooks
+import { useState, useEffect, useMemo } from "react";
+import { useLocation } from "react-router-dom";
+
 
 function GaleryContainer() {
+
+  const [stock, setStock] = useState([]);
+  const { search } = useLocation();
+  const query = useMemo(() => new URLSearchParams(search), [search]);
+
+
+  useEffect(() => {
+
+    //brings an unfiltered list on every queryparam update
+    const fetchData = async () => {
+      const response = await fetch(`/data/stock.json`); //from public
+      const newData = await response.json();
+      setStock(newData);
+    };
+    fetchData(); 
+    
+  }, [query]);
+
+  useEffect(()=>{
+    function filterByKm(chosenKm, arrayOfCars){
+      let result = [];
+      switch(chosenKm){
+          case "0":
+              result = arrayOfCars.filter(car => parseInt(car.km) === 0 )
+              break;
+          case "0_to_100":
+              result = arrayOfCars.filter(car => (parseInt(car.km) > 0 && parseInt(car.km) <= 100000 ) )
+              break;
+          case "100_plus":
+              result = arrayOfCars.filter(car => parseInt(car.km) > 100000 )
+              break;
+          case "any":
+              result = arrayOfCars.filter(car => parseInt(car.km) >= 0 )
+              break;
+          default: 
+              throw new Error(`Cannot filter by Km. $chosenKm value is: ${chosenKm}.\n
+              Check the range is contemplated in the filterByKm function`);
+      }
+      return result;
+  }
+    const filterData = () => {
+      let arrayOfCars = [...stock]
+      query.forEach( (filterValue, filterKey) => {
+        switch(filterKey){
+          case "from":
+              arrayOfCars = arrayOfCars.filter( car => parseInt(car.year) >= parseInt(filterValue) )
+              break;
+          case "up_to":
+              arrayOfCars = arrayOfCars.filter( car => parseInt(car.year) <= parseInt(filterValue) )
+              break;
+          case "price":
+              //console.log('entró price')
+              arrayOfCars = arrayOfCars.filter( car => parseInt(car.price) <= parseInt(filterValue) )
+              break;
+          case "km":
+              arrayOfCars = filterByKm(filterValue, arrayOfCars)
+              break;
+          default:
+              console.log("entró default");
+      }
+      })
+      return arrayOfCars
+    }
+
+    setStock( () => filterData() )
+
+  },[query,stock])
+
 
 
   return (
@@ -23,8 +95,8 @@ function GaleryContainer() {
           </Col>
 
           {/* Cards */}
-          <Col sm={11} md={11} lg={7} xl={7} xxl={6} className="galeryCols">
-            <GaleryMapper  />
+          <Col sm={11} md={11} lg={7} xl={6} xxl={5} className="galeryCols">
+            <GaleryMapper stock={stock}  />
           </Col>
         </Row>
       </Container>
--- src/components/mappers/GaleryMapper.jsx
+++ src/components/mappers/GaleryMapper.jsx
@@ -1,112 +1,17 @@
 import CarCard from "../presentationals/CarCard"
-import { useState, useEffect, useMemo } from "react";
-import { useLocation } from "react-router-dom";
-import stock from '../../data/stock.json'
+
+function GaleryMapper({stock}) { 
 
 
-function GaleryMapper() { 
-       
-    const [arrayOfCars, setCarsList] = useState([]);
-
-    //promise
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(stock), 500);
-    });
-    //promise assignment to state-variable
-    promise.then( dataFromPromise => {
-        setCarsList([...dataFromPromise])
-    });
-
-    const [arrayOfCarsCopy, setCarsCopyList] = useState(arrayOfCars);
-    
-    // const pathname = window.location.search
-    // const [currentUrl, setCurrentUrl] = useState(pathname)
-    // useEffect(() => {
-    //     setCurrentUrl(pathname)
-    //  }, [pathname])
-
-    //  useEffect(() => {
-    //     console.log('the url has changed!!')
-    //     console.log(currentUrl);
-    //   }, [currentUrl]); 
-
-
-    const { search } = useLocation();
-    const query = useMemo(() => new URLSearchParams(search), [search]);
-
-    useEffect(() => {
-        function filterByKm(chosenKm){
-            let result = [];
-            switch(chosenKm){
-                case "0":
-                    result = arrayOfCars.filter(car => parseInt(car.km) === 0 )
-                    break;
-                case "0_to_100":
-                    result = arrayOfCars.filter(car => (parseInt(car.km) > 0 && parseInt(car.km) <= 100000 ) )
-                    break;
-                case "100_plus":
-                    result = arrayOfCars.filter(car => parseInt(car.km) > 100000 )
-                    break;
-                case "any":
-                    result = [...arrayOfCars]
-                    break;
-                default: 
-                    throw new Error(`Cannot filter by Km. $chosenKm value is: ${chosenKm}.\n
-                    Check the range is contemplated in the filterByKm function`);
-            }
-            return result;
-        }
-        function updateCarsByCat(){
-            query.forEach( (filterValue, filterKey) => {
-
-                switch(filterKey){
-                    case "from":
-                        setCarsCopyList(arrayOfCars.filter( car => parseInt(car.year) >= parseInt(filterValue) ))
-                        break;
-                    case "up_to":
-                        setCarsCopyList(arrayOfCars.filter( car => parseInt(car.year) <= parseInt(filterValue) ))
-                        break;
-                    case "price":
-                        setCarsCopyList(arrayOfCars.filter( car => parseInt(car.price) <= parseInt(filterValue) ))
-                        break;
-                    case "km":
-                        setCarsCopyList(filterByKm(filterValue))
-                        break;
-                    default:
-                        setCarsCopyList(arrayOfCars);
-                }
-            })    
-        }
-        updateCarsByCat();
-        // query.forEach( (filterValue, filterKey) => {
-
-        //     switch(filterKey){
-        //         case "from":
-        //             setCarsCopyList(arrayOfCars.filter( car => parseInt(car.year) >= parseInt(filterValue) ))
-        //             break;
-        //         case "up_to":
-        //             setCarsCopyList(arrayOfCars.filter( car => parseInt(car.year) <= parseInt(filterValue) ))
-        //             break;
-        //         case "price":
-        //             setCarsCopyList(arrayOfCars.filter( car => parseInt(car.price) <= parseInt(filterValue) ))
-        //             break;
-        //         case "km":
-        //             setCarsCopyList(filterByKm(filterValue))
-        //             break;
-        //         default:
-        //             setCarsCopyList(arrayOfCars);
-        //     }
-        // })
-    }, [query]); 
-
-  
     return (
-        arrayOfCarsCopy.map( car => {
-            car.km = car.km.toLocaleString().replace(',', '.');
-            car.price = car.price.toLocaleString().replace(',', '.');
-            return <CarCard key={car.id} car={car} />
-            }
-        )
+        <>{
+            stock.map( car => {
+                car.km = car.km.toLocaleString().replace(',', '.');
+                car.price = car.price.toLocaleString().replace(',', '.');
+                return <CarCard key={car.id} car={car} />
+                }
+            )
+        }</>
     )
 }
 
