6862b32fa23c697e6d576d247cd9c6f10071e11b
MatiasR <noemail@noemail.com>
2023-02-27 23:53:13 -0300

    añadido target blank a los botones de ML

--- src/components/presentationals/ML.jsx
+++ src/components/presentationals/ML.jsx
@@ -3,7 +3,7 @@ import MeLi from '../../assets/mercado-libre.svg';
 function ML() {
   return (
     <div className="containerML">
-      <a href="https://www.mercadolibre.com.ar/" className="centerContent">
+      <a href="https://www.mercadolibre.com.ar/" className="centerContent" target="_blank" rel="noopener noreferrer">
           <img src={MeLi} className='logoML' alt="logo de MercadoLibre" />
       </a>
     </div>
