91fe3533999d31fa19caeb9b4e8eabd9ccebf5de
Matias <noemail@noemail.com>
2023-12-14 20:09:25 -0300

    Arreglo data fetch y mejora UX/UI -fix: GalleryContainer.jsx implementado hook useQuery de libreria react-query para solucionar error con firebase el cual a veces al primer intento la conexión era denegada. useQuery tiene la ventaja de tener reintentos ante errores. -feature: Implementado componente Error.jsx para mostrar mas claramente fallos en la conexión con firebase.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,19 +5,29 @@ import Header from "../Header/Header.jsx";
 import GalleryContainer from "../Gallery/GalleryContainer.jsx";
 import ContactUsContainer from "../Footer/ContactUsContainer.jsx";
 import Signature from "../Signature/Signature.jsx";
+// import QueryClientProvider from "../../context/QueryClientProvider.jsx";
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<GalleryContainer />} />
-        <Route path="/search" element={<GalleryContainer />} />
-      </Routes>
-      <ContactUsContainer />
-      <Signature />
-    </BrowserRouter>
-  );
-};
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<GalleryContainer />} />
+          <Route path="/search" element={<GalleryContainer />} />
+        </Routes>
+        {/* <ReactQueryDevtools /> */}
+        <ContactUsContainer />
+        <Signature />
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
+
 
 export default App;
